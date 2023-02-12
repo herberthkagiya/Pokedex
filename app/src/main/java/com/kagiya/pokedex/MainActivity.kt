51b68954074a6f2527a3bdc5b466ea3755741efd
Herberth Kagiya <tayki.heberth@gmail.com>
2023-02-12 17:10:30 -0300

    Bottom Navigation Menu Appearence Working

--- app/src/main/java/com/kagiya/pokedex/MainActivity.kt
+++ app/src/main/java/com/kagiya/pokedex/MainActivity.kt
@@ -3,15 +3,21 @@ package com.kagiya.pokedex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.kagiya.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Thread.sleep(2000)
-        val splashScreen = installSplashScreen()
+        //Start the Splash Screen
+        installSplashScreen()
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.itemIconTintList = null
     }
 }
